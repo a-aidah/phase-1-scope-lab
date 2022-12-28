2625b721d56b907dc124664f2fa4dcbd4332d565
Abdullah Omer Aidah <a.o.aidah.note9@gmail.com>
2022-12-28 05:17:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "it's bob";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Mr. bob"
+}
